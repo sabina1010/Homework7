98129cdd72919c3df44f217936f0ed4cd433ba65
sabina1010 <109016620+sabina1010@users.noreply.github.com>
2022-08-24 17:57:38 +0400

    Update Dog.java

--- hw07/src/main/java/company/pets/Dog.java
+++ hw07/src/main/java/company/pets/Dog.java
@@ -23,3 +23,30 @@ public class Dog extends Pet implements Foul {
 
     }
 }
+package company.pets;
+
+import company.Species;
+import company.abstracts.Foul;
+import company.abstracts.Pet;
+
+public class Dog extends Pet implements Foul {
+    public Dog (){
+        super.setSpecies(Species.DOG);
+
+    }
+    public Dog( String nickName, int age, int trickLevel, String[] habits){
+        super(nickName, age, trickLevel, habits);
+        super.setSpecies(Species.DOG);
+    }
+    @Override
+    public void respond() {
+        System.out.println(this.getNickName() + "Responding");
+
+    }
+
+    @Override
+    public void foul() {
+        System.out.println(this.getNickName() + "Foul");
+
+    }
+}
